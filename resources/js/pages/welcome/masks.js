5a83794746bc6a91bb0afa8b56316ce748094323
nivs <nivsoficial@gmail.com>
2023-10-18 20:26:33 -0300

    feat: numeric masks

---
+++ resources/js/pages/welcome/masks.js
@@ -0,0 +1,25 @@
+const masks = {
+    numeric: () => {
+        // Obtém uma referência para todos os elementos com a classe "numerics_values"
+        const numericInputs = document.querySelectorAll(".numerics_values");
+
+        // Itera sobre os elementos e adiciona um ouvinte de evento de entrada a cada um
+        numericInputs.forEach(function(input) {
+            input.addEventListener("input", function() {
+                // Obtém o valor do campo de entrada
+                var inputValue = this.value;
+
+                // Verifica se o valor contém letras usando uma expressão regular
+                if (/[^0-9]/.test(inputValue)) {
+                    // Se o valor contiver letras, notifique o usuário e remova as letras
+                    $.notify("Digite apenas números.", "error");
+                    this.value = inputValue.replace(/[^0-9]/g, "");
+                }
+            });
+        });
+    } 
+}
+
+
+// PARA APLICAR MASCARA NUMERICA, BASTA COLOCAR COMO CLASSE "numerics_values"
+masks.numeric();
