4b883dfecff6384d3466098ca8e2d8a66fa93806
nivs <nivsoficial@gmail.com>
2023-10-19 11:47:31 -0300

    feat/fix: atts on forms and numeric mask

--- resources/js/pages/welcome/masks.js
+++ resources/js/pages/welcome/masks.js
@@ -7,13 +7,16 @@ const masks = {
         numericInputs.forEach(function(input) {
             input.addEventListener("input", function() {
                 // Obtém o valor do campo de entrada
-                var inputValue = this.value;
+                const inputValue = this.value;
 
                 // Verifica se o valor contém letras usando uma expressão regular
                 if (/[^0-9]/.test(inputValue)) {
                     // Se o valor contiver letras, notifique o usuário e remova as letras
                     $.notify("Digite apenas números.", "error");
                     this.value = inputValue.replace(/[^0-9]/g, "");
+                    input.style.border = '1px solid red'; 
+                } else {
+                    input.style.border = '1px solid blue';  
                 }
             });
         });
--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -23,15 +23,52 @@
         </div>
         <form class="flex flex-col p-5" method="POST" action="products">
             @csrf
-            <label>Nome</label>
-            <input type="text" name="name" placeholder="Digite aqui">
-            <label>Descrição</label>
-            <input type="text" name="description" placeholder="Digite aqui">
-            <label>Preço</label>
-            <input type="text" class="numerics_values" name="price" placeholder="Digite aqui">
-            <label>Quantidade</label>
-            <input type="text" class="numerics_values" name="amount" placeholder="Digite aqui">
-            <input class="btn pointer" type="submit" placeholder="Criar">
+
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                  Nome
+                </label>
+                <input class="shadow appearance-none border border-blue-500 
+                rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight 
+                focus:outline-none focus:shadow-outline" id="name" type="name" placeholder="Digite aqui">
+                <p class="text-blue-500 text-xs italic"></p>
+            </div>
+
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
+                  Descrição
+                </label>
+                <input class="shadow appearance-none border border-blue-500 rounded w-full 
+                py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline
+                " id="description" type="description" placeholder="Digite aqui">
+                <p class="text-blue-500 text-xs italic"></p>
+            </div>
+
+
+
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="price">
+                  Preço
+                </label>
+                <input class="shadow appearance-none border border-blue-500 rounded w-full 
+                py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline
+                numerics_values" id="price" type="price" placeholder="Digite aqui">
+                <p class="text-blue-500 text-xs italic"></p>
+            </div>
+
+
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="amount">
+                  Quantidade
+                </label>
+                <input class="shadow appearance-none border border-blue-500 rounded w-full 
+                py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline
+                numerics_values" id="amount" type="amount" placeholder="Digite aqui">
+                <p class="text-blue-500 text-xs italic"></p>
+            </div>
+
+
+            <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" placeholder="Criar">
         </form>
     </div>
 
@@ -44,15 +81,53 @@
         <form class="flex flex-col p-5" method="POST" action="products/{{$product->id}}">
             @method('PUT')
             @csrf
-            <label>Nome</label>
-            <input type="text" name="name" value="{{$product->name}}" placeholder="Digite aqui">
-            <label>Descrição</label>
-            <input type="text" name="description" value="{{$product->description}}" placeholder="Digite aqui">
-            <label>Preço</label>
-            <input type="text" name="price" class="numerics_values" value="{{$product->price}}" placeholder="Digite aqui">
-            <label>Quantidade</label>
-            <input type="text" name="amount" class="numerics_values"  value="{{$product->amount}}" placeholder="Digite aqui">
-            <input class="btn pointer" type="submit" placeholder="Editar">
+        
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                  Nome
+                </label>
+                <input class="shadow appearance-none border border-blue-500 
+                rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight 
+                focus:outline-none focus:shadow-outline" id="name" value="{{$product->name}}" type="name" placeholder="Digite aqui">
+                <p class="text-blue-500 text-xs italic"></p>
+            </div>
+
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
+                  Descrição
+                </label>
+                <input class="shadow appearance-none border border-blue-500 rounded w-full 
+                py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline
+                " id="description" type="description" value="{{$product->description}}" placeholder="Digite aqui">
+                <p class="text-blue-500 text-xs italic"></p>
+            </div>
+
+
+
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="price">
+                  Preço
+                </label>
+                <input class="shadow appearance-none border border-blue-500 rounded w-full 
+                py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline
+                numerics_values" id="price" type="price" value="{{$product->price}}" placeholder="Digite aqui">
+                <p class="text-blue-500 text-xs italic"></p>
+            </div>
+
+
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="amount">
+                  Quantidade
+                </label>
+                <input class="shadow appearance-none border border-blue-500 rounded w-full 
+                py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline
+                numerics_values" id="amount" type="amount" value="{{$product->amount}}" placeholder="Digite aqui">
+                <p class="text-blue-500 text-xs italic"></p>
+            </div>
+
+
+            <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" placeholder="Editar">
+        
         </form>
     </div>
 @endif
